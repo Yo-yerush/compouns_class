b0c49a109b2c82e445f9fd8207b3345ccf9d67a2
Yonatan Yerushalmy <yonyerush@gmail.com>
2025-06-03 16:40:37 +0300

    Add files via upload

---
+++ data/create_chebi_slim.r
@@ -0,0 +1,20 @@
+setwd("C:/Users/yonatany/Migal/Rachel Amir Team - General/yonatan/chebi db/pipline_scripts/package_files_310523/")
+
+library(ontologyIndex)
+
+obo_in  <- "data/chebi.obo"
+rds_out <- "chebi_slim.rds"
+
+chebi_slim <- get_OBO(
+  obo_in,
+  extract_tags = "minimal",vector
+  propagate_relationships = c() # skip extra rels, keeps only is_a
+)
+chebi_slim$definition <- NULL
+chebi_slim$synonym    <- NULL
+chebi_slim$namespace  <- NULL
+# keep $parents / $ancestors – they’re required by get_ancestors()
+
+saveRDS(chebi_slim, rds_out, compress = "xz")
+
+chebi_db <- readRDS("chebi_slim.rds")
